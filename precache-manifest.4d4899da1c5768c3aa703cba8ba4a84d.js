4e54af4dc681d4c6f27a305b62eb1883b4153d05
elmergustavo <elmergustavo79@gmail.com>
2021-05-24 16:26:12 -0600

    Updates

---
+++ precache-manifest.4d4899da1c5768c3aa703cba8ba4a84d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f03509b87c01b3fba2f25ee41a0f12b",
+    "url": "/reactgif-practice/index.html"
+  },
+  {
+    "revision": "15773055730736334437",
+    "url": "/reactgif-practice/static/css/main.1e494564.chunk.css"
+  },
+  {
+    "revision": "06f849de45652d448f9e",
+    "url": "/reactgif-practice/static/js/2.c9f27f05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactgif-practice/static/js/2.c9f27f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15773055730736334437",
+    "url": "/reactgif-practice/static/js/main.0db4e3da.chunk.js"
+  },
+  {
+    "revision": "cd096bd786a509f0a616",
+    "url": "/reactgif-practice/static/js/runtime-main.49469e63.js"
+  }
+]);
